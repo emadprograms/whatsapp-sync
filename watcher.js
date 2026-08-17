c4b6733e3a45de4d2ed9158670f77e12c524facd
Emad <emad@example.com>
2026-08-17 10:38:39 +0300

    feat: migrate to standalone whatsapp-sync package

--- watcher.js
+++ watcher.js
@@ -1,4 +1,4 @@
-const { Client, LocalAuth, MessageMedia } = require('./index');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const path = require('path');
